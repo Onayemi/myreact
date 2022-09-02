4b77286842c1a9943fd0c9c4686821e633ae1489
Onayemi Samuel <onayemi18@gmail.com>
2022-09-02 02:39:48 +0100

    update data

--- src/components/data/books.js
+++ src/components/data/books.js
@@ -1,28 +1,25 @@
 export const books = [
     {   
         id: 1,
-        // img : 'https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL._AC_UL600_SR600,400_.jpg',
-        img : '/images/book_1.jpg',
+        img : '../assets/images/book_1.jpg',
         title : 'Lorem ipsum dolor sit amet consectetur hjghkj',
         author : 'Colleen Hoover',
     },
     {   
         id:2,
-        img : '/images/book_2.jpg',
+        img : '../assets/images/book_2.jpg',
         title : 'Lorem ipsum dolor sit amet consectetur dfsgfd',
         author : 'Onayemi Samuel',
     },
     {   
         id:3,
-        // img : 'https://images-na.ssl-images-amazon.com/images/I/71sifGleAWL._AC_UL600_SR600,400_.jpg',
-        img : '/images/book_1.jpg',
+        img : '../assets/images/book_1.jpg',
         title : 'Lorem ipsum dolor sit amet consectetur dsssd',
         author : 'Onayemi Kehinde',
     },
     {   
         id:4,
-        // img : 'https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL._AC_UL600_SR600,400_.jpg',
-        img : '/images/book_2.jpg',
+        img : '../assets/images/book_2.jpg',
         title : 'Lorem ipsum dolor sit amet gfhgghjjhjk',
         author : 'Onayemi Yemi',
     },
